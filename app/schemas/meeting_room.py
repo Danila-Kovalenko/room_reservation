d3e36551aafab53bbd5bfa76eb147f3c8469dcec
1 <margoloko@mail.ru>
2023-04-07 18:09:59 +0700

    add schemas

--- app/schemas/meeting_room.py
+++ app/schemas/meeting_room.py
@@ -3,9 +3,13 @@ from typing import Optional
 from pydantic import BaseModel, Field, validator
 
 
-class MeetingRoomCreate(BaseModel):
-    name: str = Field(..., max_lenght=100)
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_lenght=1, max_lenght=100)
     description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(..., min_lenght=1, max_lenght=100)
 
 
     @validator('name')
@@ -13,3 +17,7 @@ class MeetingRoomCreate(BaseModel):
         if value is not None:
             return value
         raise ValueError('name must not empty')
+
+
+class MeetingRoomDB(BaseModel):
+    id: int
