0f36b5bf0b4af97ca01a76e5c2381c644e04a9d4
1 <margoloko@mail.ru>
2023-04-15 17:26:39 +0700

    add migration reservation

---
+++ alembic/versions/2023_04_15_1724-76dd8a53f35d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 76dd8a53f35d
+Revises: 37b009c2ae66
+Create Date: 2023-04-15 17:24:00.149559
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '76dd8a53f35d'
+down_revision = '37b009c2ae66'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
