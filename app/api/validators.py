5be687b935575034a03e5379cdaf2fc305fbb8ad
1 <margoloko@mail.ru>
2023-04-19 20:59:29 +0700

    update validators

---
+++ app/api/validators.py
@@ -0,0 +1,25 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.crud.meeting_room import meeting_room_crud
+from app.models.meeting_room import MeetingRoom
+
+
+async def check_name_duplicate(room_name: str,
+                               session: AsyncSession,) -> None:
+    """Корутина, проверяющая уникальность полученного имени переговорки."""
+    room_id = meeting_room_crud.get_room_id_by_name(room_name, session)
+    if room_id is not None:
+        raise HTTPException(
+            status_code=422,
+            detail='Переговорка с таким именем уже существует!',
+        )
+
+async def check_meeting_room_exists(meeting_room_id: int,
+                                    session: AsyncSession) -> MeetingRoom:
+    """Корутина, проверяющая наличие переговорки."""
+    meeting_room = await meeting_room_crud.get(meeting_room_id, session)
+    if meeting_room is None:
+        raise HTTPException(status_code=404,
+                            detail='Переговорка не найдена!')
+    return meeting_room
